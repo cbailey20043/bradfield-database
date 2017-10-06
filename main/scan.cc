b76ac8da847a7e60e1b95255d0ff4563898fba3d
Cameron Bailey <cwbailey20042@gmail.com>
2017-10-06 14:23:00 -0700

    Use unordered map for RowTuple implementation

--- main/scan.cc
+++ main/scan.cc
@@ -1,30 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using std::vector;
 using std::cout;
 using std::string;
 using std::endl;
+using std::unordered_map;
 
 class RowTuple {
   public:
     RowTuple() {};
-    RowTuple(vector<string> col_names, vector<string> data) : column_headers(std::move(col_names)), row_data(std::move(data)) {}
+    RowTuple(unordered_map<string, string> input_map) : row_data(std::move(input_map)) {}
     bool is_empty();
     void print_contents();
   private:
-    // optimize this
-    vector<string> column_headers;
-    vector<string> row_data;
+    unordered_map<string, string> row_data;
 };
 
 class Iterator {
   public:
-    virtual void init() = 0;
+    //virtual void init() = 0;
     virtual void close() = 0;
     virtual RowTuple get_next() = 0;
 };
 
+// Note I believe Scan is an input / parent for Select in this example
+// FLOW: SELECT calls get_next() of scan, which is an input, applies a predicate and keeps
+// calling next on scan until select's predicate returns true
 class Scan : public Iterator {
   public:
     void init();
@@ -36,30 +39,54 @@ class Scan : public Iterator {
     void read_data();
 };
 
-class SelectionNode : public Iterator {
+class Select : public Iterator {
   public:
     void init();
     void close();
+    void set_input(Iterator *);
+    void set_predicate(bool (*predicate) (RowTuple));
     RowTuple get_next();
+  private:
+    Iterator *input;
+    bool (*predicate) (RowTuple);
 };
 
-void RowTuple::print_contents() {
-  if (column_headers.empty()) {
-    cout << "Column Names: None" << endl;
-    cout << "Data Fields: None" << endl;
-    return;
-  }
-  vector<string>::iterator col_headers_it = column_headers.begin();
-  vector<string>::iterator data_it = row_data.begin();
+/*** Implementations */
 
-  while (col_headers_it != column_headers.end() && data_it != row_data.end()) {
-    cout << "Column: " << *col_headers_it << ", " << "Value: " << *data_it << endl;
-    col_headers_it++;
-    data_it++;
-  }
-
+void Select::set_input(Iterator *it) {
+  input = it;
 }
 
+void Select::set_predicate(bool (*predicate) (RowTuple)) {
+  this->predicate = predicate;
+}
+
+RowTuple Select::get_next() {
+  RowTuple tuple;
+  // Make sure you check that predicate and input are initialized
+  // Not worrying about it right now
+  while (!(tuple = input->get_next()).is_empty()) {
+    if (predicate(tuple)) {
+      return tuple;
+    }
+  }
+  // Nothing in input satisfies predicate :(
+  return RowTuple();
+}
+
+void RowTuple::print_contents() {
+  if (row_data.empty()) {
+    cout << "Column: None" << endl; 
+    cout << "Value: None" << endl;
+  }
+
+  for (const auto& it : row_data) { // very convenient
+    cout << "Column: " << it.first << endl;
+    cout << "Value: " << it.second << endl;
+  }
+}
+
+// still works with map
 bool RowTuple::is_empty() {
   return row_data.empty();
 }
@@ -71,16 +98,16 @@ void Scan::init() {
 }
 
 void Scan::close() {
-  cout << "Scan Node closed" << "\n";
+  cout << "Scan Node closed" << endl;
 }
 
 void Scan::read_data() {
+  cout << "Reading in data from scan node" << endl;
   if (!records.empty()) {records.clear();}
+  cout << "Emptied records" << endl;
 
-  records.push_back(RowTuple({"student", "gpa"}, {"Samer", "4.0"}));
-  records.push_back(RowTuple({"student", "gpa"}, {"Vibe", "-2.0"}));
-  records.push_back(RowTuple({"student", "gpa"}, {"Cameron", "4.0009"}));
-
+  records.push_back(RowTuple({{"student", "Cameron"}, {"Sport", "Tennis"}}));
+  cout << "Records Length: " << records.size() << endl;
 }
 
 RowTuple Scan::get_next() {
@@ -94,14 +121,12 @@ RowTuple Scan::get_next() {
 }
 
 int main() {
-  cout << "Hello";
+  cout << "Starting Main Function" << endl;
   Scan scan;
   scan.init();
   RowTuple row_tuple;
-  
-  while (!(row_tuple = scan.get_next()).is_empty()) {
+  while ( !(row_tuple = scan.get_next()).is_empty()) {
     row_tuple.print_contents();
   }
-
   return (0);
 }
