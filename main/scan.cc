e6da09a89749ac0a73e500952a53d0861f86c04f
Cameron Bailey <cwbailey20042@gmail.com>
2017-10-08 10:55:42 -0700

    Add Average Node

--- main/scan.cc
+++ main/scan.cc
@@ -2,6 +2,7 @@
 #include <vector>
 #include <unordered_map>
 #include <memory>
+#include <stdlib.h>
 
 using std::vector;
 using std::cout;
@@ -12,11 +13,20 @@ using std::unique_ptr;
 
 class RowTuple {
   public:
-    RowTuple() {};
+    RowTuple() { cout << "Default RowTuple Constructor Called" << endl;}
     RowTuple(unordered_map<string, string> input_map) : row_data(std::move(input_map)) {}
 
     bool is_empty() {
       return row_data.empty();
+    }
+
+    string get_value(const string& key) {
+      auto it = row_data.find(key);
+      if (it == row_data.end()) {
+        return "";
+      }
+      return it->second;
+
     }
 
     void print_contents() {
@@ -39,6 +49,7 @@ class RowTuple {
 class Iterator {
   public:
 
+    virtual ~Iterator() = default;
     virtual void init() = 0;
     virtual void close() = 0;
     virtual RowTuple get_next() = 0;
@@ -81,9 +92,10 @@ class FileScan : public Iterator {
     RowTuple get_next() {
       if (iterator_position >= records.size()) {
         RowTuple empty_tuple;
-        return empty_tuple;
+        return RowTuple();
       }
       // Calls copy constructor
+      // maybe read from iterator instead -- future enhancement
       RowTuple row_tuple = records[iterator_position]; // same as RowTuple row_tuple(records[iterator_pos])
       iterator_position++;
       return row_tuple;
@@ -101,6 +113,7 @@ class FileScan : public Iterator {
 
       cout << "Inserting fake records " << endl;
       records.push_back(RowTuple({{"student", "Jimmy Cricket"}, {"Sport", "Tennis"}, {"id", "2"}}));
+      records.push_back(RowTuple({{"student", "Foo Bar"}, {"Sport", "blahness"}, {"id", "3"}}));
       cout << "Records read in" << endl;
     }
 };
@@ -112,48 +125,167 @@ class Select : public Iterator {
 
     void close() {}
 
-    void set_input() {}
+    void set_predicate(bool (*predicate) (RowTuple)) {
+      this->predicate = predicate;
+    }
 
-    void set_predicate(bool (*predicate) (RowTuple)) {}
+    RowTuple get_next() {
+      // right now this will only be a single input
+      // Also, add checks to make sure the predicate has been initialized
+      //
+      if (inputs.empty() || predicate == nullptr) {
+        return RowTuple();
+      }
 
-    RowTuple get_next();
+      RowTuple tuple;
+      while (!(tuple = inputs[0]->get_next()).is_empty()) {
+        if (predicate(tuple)) {
+          return tuple;
+        }
+      }
+      return RowTuple();
+    }
+
   private:
     bool (*predicate) (RowTuple);
 };
 
-/*** Implementations */
+class Count : public Iterator {
+  public:
 
-/*
-void Select::set_input(Iterator *it) {
-  input = it;
-}
+    Count() {}
 
-void Select::set_predicate(bool (*predicate) (RowTuple)) {
-  this->predicate = predicate;
-}
+    Count(string alias) : result_alias(alias) {}
 
-RowTuple Select::get_next() {
-  RowTuple tuple;
-  // Make sure you check that predicate and input are initialized
-  // Not worrying about it right now
-  while (!(tuple = input->get_next()).is_empty()) {
-    if (predicate(tuple)) {
-      return tuple;
+    void init() {}
+
+    void close() {}
+
+    void set_result_alias(const string& alias) {
+      this->result_alias = alias;
     }
-  }
-  // Nothing in input satisfies predicate :(
-  // Make sure this is doing what you want
-  return RowTuple();
+
+    RowTuple get_next() {
+      if (inputs.empty()) {
+        return RowTuple();
+      }
+
+      unique_ptr<Iterator>& input = inputs[0];
+      while (!(input->get_next()).is_empty()) {
+        num_records++;
+      }
+
+      //return RowTuple({result_alias, std::to_string(num_records)});
+      return RowTuple(std::unordered_map<string, string>{{result_alias, std::to_string(num_records)}});
+
+    }
+
+  private:
+    long num_records = 0;
+    string result_alias = "Count";
+  
+};
+
+class Average : public Iterator {
+  // Don't forget... need to take in column name
+  // Also, avg will only work work on integers at the moment
+  public:
+    Average() {}
+    Average(const string& alias) : result_alias(alias) {}
+
+    void init() {}
+    void close() {}
+
+    void set_result_alias(const string& alias) {
+      result_alias = alias;
+    }
+
+    void set_col_to_avg(const string& col_name) {
+      this->column_to_avg = col_name;
+    }
+
+    RowTuple get_next() {
+      if (inputs.empty() || column_to_avg == "") {
+        cout << "Averge: Either inputs or target col name not set" << endl;
+        return RowTuple();
+      }
+
+      unique_ptr<Iterator>& input = inputs[0];
+      RowTuple curr_tuple;
+
+      while (!(curr_tuple = input->get_next()).is_empty()) {
+        string val = curr_tuple.get_value(this->column_to_avg);
+        if (val == "") {
+          cout << "Average: No value in row_tuple matching key: " << this->column_to_avg << endl;
+          return RowTuple();
+        }
+        long converted_val = atol(val.c_str());
+        if (!converted_val) {
+          cout << "Avg: Conversion of string to long failed." << endl;
+          return RowTuple();
+        }
+        running_sum += converted_val;
+        total_count++;
+      }
+
+      double avg = running_sum/((double) total_count);
+      return RowTuple(std::unordered_map<string, string>{{result_alias, std::to_string(avg)}});
+
+    }
+
+  private:
+    string result_alias = "Average";
+    string column_to_avg = "";
+    long total_count = 0;
+    long running_sum = 0; // TODO guard against overflow
+
+    long convert_str_to_long(const string& str_val) {
+      long converted = atol(str_val.c_str());
+      return converted;
+    }
+};
+
+void query_one() {
 }
-*/
 
 int main() {
   cout << "Starting Main Function" << endl;
+  /*
   FileScan scan;
   scan.init();
   RowTuple row_tuple;
   while ( !(row_tuple = scan.get_next()).is_empty()) {
     row_tuple.print_contents();
   }
+  */
+
+  cout << "Going Further" << endl;
+
+  
+  Select* select = new Select;
+  select->get_next();
+  delete select;
+  
+
+  unique_ptr<Iterator> file_scan(new FileScan());
+  file_scan->init();
+  Count count("total_count");
+  
+  count.append_input(std::move(file_scan));
+
+  cout << "About to Print Context of count" << endl;
+  
+  count.get_next().print_contents();
+  
+
+
+
   return (0);
 }
+
+
+
+
+
+
+
